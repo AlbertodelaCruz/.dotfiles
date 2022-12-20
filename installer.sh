591f35aa3fae3b24a75783eca3ce828343713351
Alberto <acruz@alea-soluciones.com>
2022-12-20 23:21:35 +0100

    Add tmux/tmate config

--- installer.sh
+++ installer.sh
@@ -1,3 +1,5 @@
 ln -sf ~/.dotfiles/shell/bash/.bashrc ~/.bashrc
 ln -sf ~/.dotfiles/shell/bash/.bash_aliases ~/.bash_aliases
 ln -sf ~/.dotfiles/editor/vim/.vimrc ~/.vimrc
+ln -sf ~/.dotfiles/terminal/.tmate.conf ~/.tmate.conf
+ln -sf ~/.dotfiles/terminal/.tmux.conf ~/.tmux.conf
